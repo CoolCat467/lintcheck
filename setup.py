8dec7fcffef0aaa20e5fe865c8d6b8453f1b6df4
CoolCat467 <52022020+CoolCat467@users.noreply.github.com>
2022-06-08 13:31:29 -0500

    Forgot setup files

--- setup.py
+++ setup.py
@@ -11,10 +11,10 @@ import pathlib
 
 here = pathlib.Path(__file__).parent.resolve()
 
-version = '0.2.0'
+version = '0.2.2'
 # Get the long description from the README file
 long_description = (here / 'README.md').read_text(encoding='utf-8')
-install_requires = ['pylint', 'tkinter', 'idlelib', 'typing']
+install_requires = ['pylint']
 
 # Arguments marked as "Required" below must be included for upload to PyPI.
 # Fields marked as "Optional" may be commented out.
@@ -102,6 +102,9 @@ setup(
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate you support Python 3. These classifiers are *not*
         # checked by 'pip install'. See instead 'python_requires' below.
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3 :: Only',
@@ -134,7 +137,7 @@ setup(
     # 'Programming Language' classifiers above, 'pip install' will check this
     # and refuse to install the project if the version does not match. See
     # https://packaging.python.org/guides/distributing-packages-using-setuptools/#python-requires
-    python_requires='>=3.9, <4',
+    python_requires='>=3.6, <4',
 
     # This field lists other packages that your project depends on to run.
     # Any package you put here will be installed by pip when your project is
